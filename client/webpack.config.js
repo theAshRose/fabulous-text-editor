2e0bed171d85b8985d56cd1d79fc3c9a6473040d
theDomconrad <them.and.us.2013@gmail.com>
2022-12-01 19:20:02 -0800

    fixed plugins, added rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,34 +6,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-new HtmlWebpackPlugin({
-  template: './index.html',
-  title: 'jate',
-}),
 
-new InjectManifest({
-  swSrc: './src-sw.js',
-  swDest: 'src-sw.js',
-}),
-
-new WebpackPwaManifest({
-  fingerprints: false,
-  inject: true,
-  name: 'jate',
-  short_name: 'jate',
-  description: 'jate',
-  background_color: '#1b89bc',
-  theme_color: '#225ca3',
-  start_url: '/',
-  publicPath: '/',
-  icons: [
-      {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-      },
-  ],
-}),
 
 module.exports = () => {
   return {
@@ -47,12 +20,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'jate',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'jate',
+        short_name: 'jate',
+        description: 'jate',
+        background_color: '#1b89bc',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        },
       ],
     },
   };
