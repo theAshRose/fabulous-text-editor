305246df4eb6caa21a5f79157201a8866d63df37
theDomconrad <them.and.us.2013@gmail.com>
2022-12-01 19:34:59 -0800

    database redo

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const trans = jateDb.transaction('jate', 'readwrite');
+  const store = trans.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('New things saved', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const trans = jateDb.transaction('jate', 'readonly');
+  const store = trans.objectStore('jate');
+  const request = store.get(1); //had to change from getAll to get(1)
+  const result = await request;
+  console.log('retrieving the things', result.value)
+  return result?.value
+};
 
 initdb();
