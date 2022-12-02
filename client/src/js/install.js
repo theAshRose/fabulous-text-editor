5f35b6740051a04b6d8e3269f01bc8ab0217eb0a
theDomconrad <them.and.us.2013@gmail.com>
2022-12-01 19:29:03 -0800

    added buttInstall functions

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,19 @@
-const butInstall = document.getElementById('buttonInstall');
-
 // Logic for installing the PWA
+const buttInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    buttInstall.classList.toggle('hidden', false);
+});
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
+buttInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) return;
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    buttInstall.classList.toggle('hidden', true);
+});
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
